c685cc2617a6f88f8a1e0185aa14acb4d5917366
Ejibode Ibraheem <iejibode@gmail.com>
2026-01-15 14:57:32 +0100

    Integrate Kommunicate chat widget in layout
    Add Kommunicate chat widget script to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,3 +28,33 @@ export default function RootLayout({
     </html>
   )
 }
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, m){
+                var kommunicateSettings = {
+                  "appId": "1942e243204bc19d23715ed36e7001ecd"
+                };
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                var h = document.getElementsByTagName("head")[0];
+                h.appendChild(s);
+                window.kommunicate = m;
+                m._globals = kommunicateSettings;
+              })(document, window.kommunicate || {});
+            `,
+          }}
+        />
+      </body>
+    </html>
+  )
+}
